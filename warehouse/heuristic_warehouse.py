397e768f1d2a8a90dde6df3ccf7c1901abb4b42f
Raoul Neves <raoulneves@gmail.com>
2023-05-22 17:08:02 +0100

    clean up v2

--- warehouse/heuristic_warehouse.py
+++ warehouse/heuristic_warehouse.py
@@ -10,12 +10,14 @@ class HeuristicWarehouse(Heuristic[WarehouseProblemSearch, WarehouseState]):
 
     def compute(self, state: WarehouseState) -> float:
         # TODO
+
+        # The heuristic value is the Manhattan distance between the forklift and the exit
         forklift_pos = (state.line_forklift, state.column_forklift)
         exit_pos = (state.line_exit, state.column_exit)
 
         heuristic_value = abs(forklift_pos[0] - exit_pos[0]) + abs(forklift_pos[1] - exit_pos[1])
 
         return heuristic_value
+
     def __str__(self):
         return "HeuristicWarehouse"
-
