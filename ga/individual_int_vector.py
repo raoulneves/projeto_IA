d02a9550ce28e271e3465e5b27304bde1056525a
Raoul Neves <raoulneves@gmail.com>
2023-06-15 00:31:40 +0100

    int vector ind genome

--- ga/individual_int_vector.py
+++ ga/individual_int_vector.py
@@ -1,5 +1,9 @@
-
+import random
 from abc import abstractmethod
+
+import numpy as np
+
+from ga.genetic_algorithm import GeneticAlgorithm
 from ga.problem import Problem
 from ga.individual import Individual
 
@@ -8,6 +12,20 @@ class IntVectorIndividual(Individual):
     def __init__(self, problem: Problem, num_genes: int):
         super().__init__(problem, num_genes)
         # TODO
+        self.genome = np.full(num_genes, 0, dtype=int)
+
+    def initialize(self, prob1s: float):
+        # Create pool of product numbers
+        # Generate the genome by randomly assigning products to agents
+        # genome = [(product, random.choice(agents)) for product in products]
+        for i, product in enumerate(self.problem.products * self.problem.agents):
+            agent_id = random.choice(self.problem.agents)
+            if i == 0:
+                self.genome[i] = product
+                self.genome[i + 1] = agent_id
+            else:
+                self.genome[i + 1] = product
+                self.genome[i + 2] = agent_id
 
     def swap_genes(self, other, index: int):
         aux = self.genome[index]
