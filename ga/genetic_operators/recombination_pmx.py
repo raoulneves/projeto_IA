beec4b854df6ddf6ebc9b9e0552463561568cf8c
Raoul Neves <raoulneves@gmail.com>
2023-06-24 23:43:56 +0100

    dead end, new genome better

--- ga/genetic_operators/recombination_pmx.py
+++ ga/genetic_operators/recombination_pmx.py
@@ -14,40 +14,42 @@ class RecombinationPMX(Recombination):
         cut2 = GeneticAlgorithm.rand.randint(0, num_genes - 1)
         if cut2 < cut1:
             cut1, cut2 = cut2, cut1
+
         mapping1 = {}
         mapping2 = {}
         for i in range(cut1, cut2 + 1):
-            mapping1[i] = ind2.genome[i]
-            mapping2[i] = ind1.genome[i]
+            mapping1[tuple(ind1.genome[i])] = tuple(ind2.genome[i])
+            mapping2[tuple(ind2.genome[i])] = tuple(ind1.genome[i])
 
-        # Create the first child by copying the selected segment from ind1
-        # and filling in the remaining genes from ind2
-        child1 = [[-1, -1] for i in range(len(ind1.genome))]
-        for i in range(cut1, cut2 + 1):
-            child1[i][1] = ind1.genome[i][1]
-        for i in range(len(ind1.genome)):
-            if child1[i][1] == -1:
-                gene = ind2.genome[i][1]
-                while i in mapping1:
-                    gene = mapping1[i][1]
-                child1[i][1] = gene
+        child1 = [-1] * num_genes
+        child2 = [-1] * num_genes
 
-        # Create the second child by copying the selected segment from ind2
-        # and filling in the remaining genes from ind1
-        child2 = [[-1, -1] for i in range(len(ind1.genome))]
-        for i in range(cut1, cut2 + 1):
-            child2[i][1] = ind2.genome[i][1]
-        for i in range(len(ind1.genome)):
-            if child2[i][1] == -1:
-                gene = ind1.genome[i][1]
-                while i in mapping2:
-                    gene = mapping2[i][1]
-                child2[i][1] = gene
+        # Copy the selected segment from ind1 to child1
+        child1[cut1:cut2+1] = ind1.genome[cut1:cut2+1]
 
-        ind1.genome = child2
-        print("CHILD 1: ", child1)
-        ind2.genome = child1
-        print("CHILD 2: ", child2)
+        # Copy the selected segment from ind2 to child2
+        child2[cut1:cut2+1] = ind2.genome[cut1:cut2+1]
+
+        # Fill in the remaining genes for child1
+        for i in range(num_genes):
+            if child1[i] == -1:
+                gene = ind2.genome[i]
+                while tuple(gene) in mapping1:
+                    gene = mapping1[tuple(gene)]
+                child1[i] = gene
+
+        # Fill in the remaining genes for child2
+        for i in range(num_genes):
+            if child2[i] == -1:
+                gene = ind1.genome[i]
+                while tuple(gene) in mapping2:
+                    gene = mapping2[tuple(gene)]
+                child2[i] = gene
+
+        ind1.genome = child1
+        print("child1: ", child1)
+        ind2.genome = child2
+        print("child2: ", child2)
 
     def __str__(self):
         return "PMX recombination (" + f'{self.probability}' + ")"
