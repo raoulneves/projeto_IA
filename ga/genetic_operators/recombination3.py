074efde895f434150c79b362a4a745206065dd31
Raoul Neves <raoulneves@gmail.com>
2023-06-24 23:41:31 +0100

    big fix recomb3 + debug

--- ga/genetic_operators/recombination3.py
+++ ga/genetic_operators/recombination3.py
@@ -9,18 +9,35 @@ class Recombination3(Recombination):
         super().__init__(probability)
 
     def recombine(self, ind1: Individual, ind2: Individual) -> None:
-        cut1 = GeneticAlgorithm.rand.randint(0, ind1.num_genes - 1)
-        cut2 = GeneticAlgorithm.rand.randint(cut1 + 1, ind1.num_genes)
-        if cut1 > cut2:
+        num_genes = len(ind1.genome)
+        cut1 = GeneticAlgorithm.rand.randint(0, num_genes - 1)
+        cut2 = GeneticAlgorithm.rand.randint(0, num_genes - 1)
+        if cut2 < cut1:
             cut1, cut2 = cut2, cut1
+        print("------NEW RECOMBINATION------")
+        print("Cut1: ", cut1)
+        print("Cut2: ", cut2)
+
+
         # Swap genes between cut points and create mappings
         mapping1 = {}
         mapping2 = {}
+        print("->BEFORE")
+        print("Ind1: ", ind1.genome)
+        print("Ind2: ", ind2.genome)
 
-        for i in range(cut1, cut2):
+
+
+        for i in range(cut1, cut2 + 1):
             mapping1[ind2.genome[i]] = ind1.genome[i]
             mapping2[ind1.genome[i]] = ind2.genome[i]
             ind1.genome[i], ind2.genome[i] = ind2.genome[i], ind1.genome[i]
+
+        print("->AFTER")
+        print("Mapping1: ", mapping1)
+        print("Mapping2: ", mapping2)
+        print("Ind1: ", ind1.genome)
+        print("Ind2: ", ind2.genome)
 
         # Update the remaining genes to eliminate duplicates
         for i in range(0, cut1):
@@ -34,7 +51,7 @@ class Recombination3(Recombination):
                 gene = mapping2[gene]
             ind2.genome[i] = gene
 
-        for i in range(cut2, ind1.num_genes):
+        for i in range(cut2 + 1, num_genes):
             gene = ind1.genome[i]
             while gene in mapping1:
                 gene = mapping1[gene]
@@ -44,6 +61,11 @@ class Recombination3(Recombination):
             while gene in mapping2:
                 gene = mapping2[gene]
             ind2.genome[i] = gene
+
+        print("->FINAL")
+
+        print("Ind1: ", ind1.genome)
+        print("Ind2: ", ind2.genome)
 
     def __str__(self):
         return "Recombination 3 (" + f'{self.probability}' + ")"
