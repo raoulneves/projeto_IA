b01e5b1aff66a5e0bec7b4f6840be7af0ed040ec
Raoul Neves <raoulneves@gmail.com>
2023-06-24 23:41:53 +0100

    removed debug

--- ga/genetic_operators/recombination3.py
+++ ga/genetic_operators/recombination3.py
@@ -14,30 +14,15 @@ class Recombination3(Recombination):
         cut2 = GeneticAlgorithm.rand.randint(0, num_genes - 1)
         if cut2 < cut1:
             cut1, cut2 = cut2, cut1
-        print("------NEW RECOMBINATION------")
-        print("Cut1: ", cut1)
-        print("Cut2: ", cut2)
-
 
         # Swap genes between cut points and create mappings
         mapping1 = {}
         mapping2 = {}
-        print("->BEFORE")
-        print("Ind1: ", ind1.genome)
-        print("Ind2: ", ind2.genome)
-
-
 
         for i in range(cut1, cut2 + 1):
             mapping1[ind2.genome[i]] = ind1.genome[i]
             mapping2[ind1.genome[i]] = ind2.genome[i]
             ind1.genome[i], ind2.genome[i] = ind2.genome[i], ind1.genome[i]
-
-        print("->AFTER")
-        print("Mapping1: ", mapping1)
-        print("Mapping2: ", mapping2)
-        print("Ind1: ", ind1.genome)
-        print("Ind2: ", ind2.genome)
 
         # Update the remaining genes to eliminate duplicates
         for i in range(0, cut1):
@@ -61,11 +46,6 @@ class Recombination3(Recombination):
             while gene in mapping2:
                 gene = mapping2[gene]
             ind2.genome[i] = gene
-
-        print("->FINAL")
-
-        print("Ind1: ", ind1.genome)
-        print("Ind2: ", ind2.genome)
 
     def __str__(self):
         return "Recombination 3 (" + f'{self.probability}' + ")"
