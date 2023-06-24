c39178d09a567eba058e0cd2ea3b40d997a2c236
Raoul Neves <raoulneves@gmail.com>
2023-06-24 23:08:46 +0100

    RECOMBINATION3 WORKING YEAHHH

--- ga/genetic_operators/recombination2.py
+++ ga/genetic_operators/recombination2.py
@@ -10,25 +10,16 @@ class Recombination2(Recombination):
 
     def recombine(self, ind1: Individual, ind2: Individual) -> None:
         cut = GeneticAlgorithm.rand.randint(0, ind1.num_genes - 1)
-        print("------NEW RECOMBINATION------")
-        print("Cut: ", cut)
         # Swap genes up to the cut point and create a mapping
         mapping1 = {}
         mapping2 = {}
-        print("->BEFORE")
-        print("Ind1: ", ind1.genome)
-        print("Ind2: ", ind2.genome)
+
         for i in range(cut + 1):
             mapping1[ind2.genome[i]] = ind1.genome[i]
             mapping2[ind1.genome[i]] = ind2.genome[i]
             ind1.genome[i], ind2.genome[i] = ind2.genome[i], ind1.genome[i]
 
-        print("->AFTER")
-        print("Mapping1: ", mapping1)
-        print("Mapping2: ", mapping2)
-        print("Ind1: ", ind1.genome)
-        print("Ind2: ", ind2.genome)
-        # Update the remaining genes to eliminate duplicates (excluding 999)
+        # Update the remaining genes to eliminate duplicates
         for i in range(cut + 1, len(ind1.genome)):
             gene = ind1.genome[i]
             while gene in mapping1:
@@ -40,10 +31,6 @@ class Recombination2(Recombination):
                 gene = mapping2[gene]
             ind2.genome[i] = gene
 
-        print("->FINAL")
-
-        print("Ind1: ", ind1.genome)
-        print("Ind2: ", ind2.genome)
 
     def __str__(self):
         return "Recombination 2 (" + f'{self.probability}' + ")"
--- ga/genetic_operators/recombination3.py
+++ ga/genetic_operators/recombination3.py
@@ -1,3 +1,4 @@
+from ga.genetic_algorithm import GeneticAlgorithm
 from ga.genetic_operators.recombination import Recombination
 from ga.individual import Individual
 
@@ -8,8 +9,41 @@ class Recombination3(Recombination):
         super().__init__(probability)
 
     def recombine(self, ind1: Individual, ind2: Individual) -> None:
-        # TODO
-        pass
+        cut1 = GeneticAlgorithm.rand.randint(0, ind1.num_genes - 1)
+        cut2 = GeneticAlgorithm.rand.randint(cut1 + 1, ind1.num_genes)
+        if cut1 > cut2:
+            cut1, cut2 = cut2, cut1
+        # Swap genes between cut points and create mappings
+        mapping1 = {}
+        mapping2 = {}
+
+        for i in range(cut1, cut2):
+            mapping1[ind2.genome[i]] = ind1.genome[i]
+            mapping2[ind1.genome[i]] = ind2.genome[i]
+            ind1.genome[i], ind2.genome[i] = ind2.genome[i], ind1.genome[i]
+
+        # Update the remaining genes to eliminate duplicates
+        for i in range(0, cut1):
+            gene = ind1.genome[i]
+            while gene in mapping1:
+                gene = mapping1[gene]
+            ind1.genome[i] = gene
+
+            gene = ind2.genome[i]
+            while gene in mapping2:
+                gene = mapping2[gene]
+            ind2.genome[i] = gene
+
+        for i in range(cut2, ind1.num_genes):
+            gene = ind1.genome[i]
+            while gene in mapping1:
+                gene = mapping1[gene]
+            ind1.genome[i] = gene
+
+            gene = ind2.genome[i]
+            while gene in mapping2:
+                gene = mapping2[gene]
+            ind2.genome[i] = gene
 
     def __str__(self):
         return "Recombination 3 (" + f'{self.probability}' + ")"
